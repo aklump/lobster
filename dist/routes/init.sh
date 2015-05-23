cfb40ae1d5a4ced65d9f04d8ec7bbdb878ac81e6
aklump <sourcecode@intheloftstudios.com>
2015-05-22 17:59:41 -0700

    Removed warning about being inside $HOME

--- dist/routes/init.sh
+++ dist/routes/init.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # 
 # @file
-# Initialize the current directory as a PSP instance
+# Initialize the current directory by copying the app's configuration file into
+# the current directory.
 
 if [ -f "$lobster_app_config" ]; then
   lobster_error "$PWD is already initialized."
   lobster_theme "failed"
 fi
 
-if [ "$lobster_pwd_root" ]; then
-  lobster_warning "You are currently inside a project (root is $lobster_pwd_root)."
+# Because we use ~/.*config it would be silly to warn them if they were finding
+# a conflict which is the home folder.
+if [ "$lobster_pwd_root" ] && [ "$lobster_pwd_root" != "$HOME" ]; then
+  lobster_warning "You are currently in a subdirectory of an initialized directory ($lobster_pwd_root)."
   if ! lobster_confirm "Are you sure?"; then
     lobster_theme "failed"
   fi
 fi
 
-cp "$lobster_app_root/$lobster_app_config" "$lobster_app_config"
-lobster_success "Success"
+cp "$lobster_app_root/$lobster_app_config" "$lobster_app_config" && lobster_success "Success"
