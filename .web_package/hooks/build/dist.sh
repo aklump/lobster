5fada14ee40cf7e71a165fd2e9a860f71a697f07
aklump <sourcecode@intheloftstudios.com>
2016-06-16 17:48:16 -0700

    updated loft docs

--- .web_package/hooks/build/dist.sh
+++ .web_package/hooks/build/dist.sh
@@ -3,18 +3,19 @@
 # @file
 # Handles build processes
 
-test -s $7/dist && rm $7/dist
-
+# Make sure dist is not a symlink and exists as a folder
+! test -d $7/dist && rm $7/dist
 if [ ! -d $7/dist ]; then
   mkdir $7/dist
 fi
 
+# Add everything needed to the dist folder.
 rsync -a --delete $7/src/ $7/dist/
 cp lobster.info $7/dist/
 cp LICENSE.txt $7/dist/
 
 # Copy dist onto the example app
--d $7/example_app/lib/lobster/dist || mkdir -p $7/example_app/lib/lobster/dist
+test -d $7/example_app/lib/lobster/dist || mkdir -p $7/example_app/lib/lobster/dist
 rsync -a --delete $7/dist/ $7/example_app/lib/lobster/dist/
 
 # Copy config files to the example app
