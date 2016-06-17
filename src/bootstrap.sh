458784629c598db84f8a5546c16f566061d1e785
aklump <sourcecode@intheloftstudios.com>
2016-06-16 18:02:59 -0700

    Fixed missing lobster_op in boostrap includes.

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -129,11 +129,13 @@ export LOBSTER_INSTANCE_ROOT
 export LOBSTER_TMPDIR
 
 # Run the app's config at the last moment to maximum variable access.
+
 lobster_load_config "$lobster_app_config"
 
 # Bootstrap the project layer
+lobster_op=${lobster_args[0]}
 lobster_include 'bootstrap'
 
-# Keep this here to allow app bootstrap to modify the arguments before we assign the op.
+# Keep this reassign here to allow app bootstrap to modify the arguments before we assign the op.
 lobster_op=${lobster_args[0]}
 lobster_include 'functions'
